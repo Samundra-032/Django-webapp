dc352d3e87ef8d81205fb83e1d9df14e2527efbf
Samundra Acharya <54012620+Samundra-032@users.noreply.github.com>
2024-08-07 12:44:44 +0545

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -36,8 +36,8 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
